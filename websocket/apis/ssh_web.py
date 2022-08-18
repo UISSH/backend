253b00fc6f1a6ed0d8c4c111e5c11559b152a790
zmaplex <zmaplex@gmail.com>
2022-08-18 17:49:31 +0800

    fix bug

--- websocket/apis/ssh_web.py
+++ websocket/apis/ssh_web.py
@@ -81,7 +81,7 @@ class SshWebConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps({'message': msg}))
 
         if not hasattr(self.client, 'get_transport'):
-            self.close(500)
+            self.close()
 
         self.ssh_session = self.client.get_transport().open_session()  # 成功连接后获取ssh通道
         self.ssh_session.get_pty()  # 获取一个终端
