a3c358870e665f44ea75b7e93e11981233a6bd70
zmaplex <zmaplex@gmail.com>
2022-09-17 14:05:43 +0800

    Add restrict access.

--- common/views.py
+++ common/views.py
@@ -1,11 +1,20 @@
 import os
 import pathlib
 
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.response import Response
+from django.conf import settings
 
 
 def index(request):
+    request_host = request.headers.get('host')
+    cors_allowed_origins = settings.CORS_ALLOWED_ORIGINS
+    host = f"{request.scheme}://{request_host}"
+    if host not in cors_allowed_origins:
+        return HttpResponse(pathlib.Path("./common/html/404.html").open(), status=404)
+
+
     static_index = pathlib.Path('./static/common/index.html')
     template_index = pathlib.Path('./common/templates/common/index.html')
 
@@ -16,6 +25,4 @@ def index(request):
         os.system(f'ln -s {static_index.absolute()}  {template_index.absolute()}')
         return render(request, 'common/index.html')
     else:
-        return Response("Frontend static files not found.",404)
-
-
+        return HttpResponse("Frontend static files not found.",status= 404)
