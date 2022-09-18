c9f7b7879938663d8b762411b5ab12349f25c75c
zmaplex <zmaplex@gmail.com>
2022-09-18 18:35:31 +0800

    Fix permission denied bug. #9

--- webdav/utils/webdav_server.sh
+++ webdav/utils/webdav_server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 rm -rf /run/ui-ssh-webdav.socket
-/usr/local/uissh/webdav/webdav --config /usr/local/uissh/webdav/config.yaml
+/usr/local/uissh/webdav/webdav --config /usr/local/uissh/webdav/config.yaml &
+sleep 3
 chown www-data.www-data /run/ui-ssh-webdav.socket
