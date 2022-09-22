412d7ae9f0b594c39b1bc7590830d76784f85a1a
zmaplex <zmaplex@gmail.com>
2022-09-22 19:14:48 +0800

    Fix ftp server config bug.

--- ftpserver/admin.py
+++ ftpserver/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from ftpserver.models import FtpServerModel
+
+
 # Register your models here.
+@admin.register(FtpServerModel)
+class FtpServerModelAdmin(admin.ModelAdmin):
+    pass
