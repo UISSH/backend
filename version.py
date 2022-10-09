ae705f9928db6e106b09176c0624b2f0d9aa4d09
zmaplex <zmaplex@gmail.com>
2022-10-09 19:11:37 +0800

    Updated: #19

--- version.py
+++ version.py
@@ -1,19 +1,26 @@
-"""
-https://semver.org/
-"""
 import os
 
-CURRENT_VERSION = 'v0.0.3-alpha'
-FRONTED_MINIMUM_VERSION = 'v0.0.3-alpha'
+CURRENT_VERSION = '0.0.3-alpha'
+FRONTED_MINIMUM_VERSION = '0.0.3-alpha'
 MIRROR_URL = 'https://mirror-cloudflare.uissh.com/'
-FRONTEND_URL = f"{MIRROR_URL}https://github.com/UISSH/frontend/releases/download/{FRONTED_MINIMUM_VERSION}/django_spa.zip"
+FRONTEND_URL = f"{MIRROR_URL}https://github.com/UISSH/frontend/releases/download/v{FRONTED_MINIMUM_VERSION}/django_spa.zip"
 
 PROJECT_DIR = "/usr/local/uissh"
 BACKEND_DIR = f"{PROJECT_DIR}/backend"
+PYTHON_INTERPRETER = f'{BACKEND_DIR}/venv/bin/python'
 
 
 def cmd(command):
     os.system(command)
+
+
+def upgrade_backend_project():
+    cmd(f'cd {BACKEND_DIR} && wget https://github.com/UISSH/backend/archive/refs/tags/v{CURRENT_VERSION}.zip')
+    cmd(f'cd {BACKEND_DIR} && unzip v{CURRENT_VERSION}.zip')
+    cmd(f'cd {BACKEND_DIR} && cp backend-{CURRENT_VERSION}/* ./ -r')
+    cmd(f'cd {BACKEND_DIR} && rm  backend-{CURRENT_VERSION}  *.zip -rf')
+    cmd(f'{PYTHON_INTERPRETER} {BACKEND_DIR}/manage.py makemigrations')
+    cmd(f'{PYTHON_INTERPRETER} {BACKEND_DIR}/manage.py migrate')
 
 
 def upgrade_front_project():
@@ -21,3 +28,8 @@ def upgrade_front_project():
     cmd(f'cd {BACKEND_DIR}/static && unzip django_spa.zip')
     cmd(f'cd {BACKEND_DIR}/static && mv spa common')
     cmd(f'cd {PROJECT_DIR} && rm -rf backend-release-* *.zip')
+
+
+if __name__ == '__main__':
+    upgrade_backend_project()
+    upgrade_front_project()
