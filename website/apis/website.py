d51da9b0f69d0a3f87aed9293adc94f540d27ba4
zmaplex <zmaplex@gmail.com>
2023-04-21 12:17:49 +0000

    Added: get website default nginx config #10

--- website/apis/website.py
+++ website/apis/website.py
@@ -2,7 +2,7 @@ import pathlib
 import traceback
 
 from django.db import transaction
-from drf_spectacular.utils import extend_schema, OpenApiParameter
+from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.request import Request
@@ -16,14 +16,19 @@ from common.models import User
 from common.serializers.operating import OperatingResSerializer
 from website.applications.core.dataclass import BaseSSLCertificate
 from website.models import Website
-from website.serializers.website import WebsiteModelSerializer, WebsiteConfigSerializer, WebsiteDomainConfigSerializer
+from website.serializers.website import (
+    DefaultWebsuteConfigSerializer,
+    WebsiteConfigSerializer,
+    WebsiteDomainConfigSerializer,
+    WebsiteModelSerializer,
+)
 from website.utils.certificate import issuing_certificate
 from website.utils.domain import domain_is_resolved, find_domain_in_nginx
 
 
 class WebsiteView(BaseModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
-    queryset = Website.objects.select_related('user').all()
+    queryset = Website.objects.select_related("user").all()
     serializer_class = WebsiteModelSerializer
 
     def get_queryset(self):
@@ -37,12 +42,15 @@ class WebsiteView(BaseModelViewSet):
     def create(self, request, *args, **kwargs):
         return super(WebsiteView, self).create(request, *args, **kwargs)
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
     # @extend_schema(responses= todo add schema)
-    @action(methods=['get'], detail=True)
+    @action(methods=["get"], detail=True)
     def get_ssl_info(self, request, *args, **kwargs):
         instance = self.get_object()
-        certificate_path = instance.ssl_config['path']['certificate']
-        if certificate_path != '' and pathlib.Path(certificate_path).exists():
+        certificate_path = instance.ssl_config["path"]["certificate"]
+        if certificate_path != "" and pathlib.Path(certificate_path).exists():
             try:
                 cert = BaseSSLCertificate.get_certificate(certificate_path)
                 return Response(cert.__dict__)
@@ -51,33 +59,34 @@ class WebsiteView(BaseModelViewSet):
                 return Response(e, status=500)
 
         else:
-            return Response('Not found', status=404)
+            return Response("Not found", status=404)
 
-    @action(methods=['post'], detail=True, serializer_class=WebsiteDomainConfigSerializer)
+    @action(
+        methods=["post"], detail=True, serializer_class=WebsiteDomainConfigSerializer
+    )
     def update_domain(self, request, *args, **kwargs):
         obj: Website = self.get_object()
-        serializer = WebsiteDomainConfigSerializer(
-            data=request.data, instance=obj)
+        serializer = WebsiteDomainConfigSerializer(data=request.data, instance=obj)
         if serializer.is_valid(raise_exception=True):
             data = serializer.save()
             return Response(data)
 
-    @action(methods=['post'], detail=True, serializer_class=WebsiteConfigSerializer)
+    @action(methods=["post"], detail=True, serializer_class=WebsiteConfigSerializer)
     def update_web_config(self, request, *args, **kwargs):
         obj = self.get_object()
         serializer = WebsiteConfigSerializer(data=request.data, instance=obj)
         if serializer.is_valid(raise_exception=True):
             data = serializer.save()
-            return Response({'web_server_config': data})
+            return Response({"web_server_config": data})
 
-    @action(methods=['post'], detail=True, serializer_class=OperatingResSerializer)
+    @action(methods=["post"], detail=True, serializer_class=OperatingResSerializer)
     def enable_ssl(self, request, *args, **kwargs):
         obj: Website = self.get_object()
         op = BaseOperatingRes()
 
         p = issuing_certificate(obj)
         if p.returncode != 0:
-            op.msg = 'issue certificate error:\n' + format_completed_process(p)
+            op.msg = "issue certificate error:\n" + format_completed_process(p)
             op.set_failure()
             obj.get_application().toggle_ssl(False)
         else:
@@ -87,7 +96,7 @@ class WebsiteView(BaseModelViewSet):
 
         return Response(op.json())
 
-    @action(methods=['post'], detail=True, serializer_class=OperatingResSerializer)
+    @action(methods=["post"], detail=True, serializer_class=OperatingResSerializer)
     def disable_ssl(self, request, *args, **kwargs):
         op = BaseOperatingRes()
         obj: Website = self.get_object()
@@ -96,29 +105,35 @@ class WebsiteView(BaseModelViewSet):
         op.set_success()
         return Response(op.json())
 
-    @extend_schema(parameters=[OpenApiParameter(name='domain', type=str)])
+    @extend_schema(parameters=[OpenApiParameter(name="domain", type=str)])
     @action(methods=["get"], detail=False, serializer_class=OperatingResSerializer)
     def verify_dns_records(self, request: Request, *args, **kwargs):
         """
         如果 UISSH 没有启用SSL直接返回失败，否则进行域名解析判断流程。
-        
-        If SSL is not enabled for UISSH, it returns a failure directly; 
+
+        If SSL is not enabled for UISSH, it returns a failure directly;
         otherwise, the domain name resolution and verification process are carried out.
         """
         if not find_domain_in_nginx():
             op = BaseOperatingRes()
             # op.set_failure("UISSH 没有启用SSL，无法进行域名解析判断。")
-            op.set_failure("Without enabling SSL " 
-                           "UISSH cannot perform domain name resolution and verification.")
+            op.set_failure(
+                "Without enabling SSL "
+                "UISSH cannot perform domain name resolution and verification."
+            )
             return Response(op.json())
 
         domain = request.query_params.get("domain")
         op = domain_is_resolved(domain, request)
         return Response(op.json())
 
-    @extend_schema(parameters=[OpenApiParameter(name='domain', type=str)])
+    @extend_schema(parameters=[OpenApiParameter(name="domain", type=str)])
     @action(methods=["get"], detail=False, serializer_class=OperatingResSerializer)
     def domain_records(self, request: Request, *args, **kwargs):
+        """
+        用于域名解析判断流程中的第二步，获取随机字符串用来核对。
+        It is used in the second step of the domain name resolution judgment process to obtain random strings for checking.
+        """
         _cache = IBaseCache()
         op = BaseOperatingRes()
         op.set_processing()
@@ -128,5 +143,17 @@ class WebsiteView(BaseModelViewSet):
         op.set_success()
         return Response(op.json())
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    @action(methods=["get"], detail=True, serializer_class=DefaultWebsuteConfigSerializer)
+    def get_default_nginx_config(self, request, *args, **kwargs):
+        """
+        获取提供给用户的默认 nginx 配置文件。
+        Get the default nginx configuration file provided to the user.
+        """
+        obj: Website = self.get_object()
+        serializer = DefaultWebsuteConfigSerializer(
+            instance=obj, data={"web_server_config": obj.valid_web_server_config}
+        )
+
+        if serializer.is_valid(raise_exception=True):
+            data = serializer.save()
+            return Response(data)
--- website/applications/app_factory.py
+++ website/applications/app_factory.py
@@ -8,9 +8,10 @@ from loguru import logger
 
 from base.utils.logger import plog
 from website.applications.core.application import Application, Storage
-from website.applications.core.dataclass import NewWebSiteConfig, WebServerTypeEnum
+from website.applications.core.dataclass import (NewWebSiteConfig,
+                                                 WebServerTypeEnum)
 
-filepath = 'website/applications/app'
+filepath = "website/applications/app"
 
 
 class AppFactory:
@@ -22,10 +23,10 @@ class AppFactory:
         for _key in AppFactory.MODULES:
             data = AppFactory.MODULES[_key]
             try:
-                module: Application = data.pop('module')
+                module: Application = data.pop("module")
                 data["attr"] = module.get_app_parameter()
             except:
-                logger.log('ERROR', traceback.format_exc())
+                logger.log("ERROR", traceback.format_exc())
                 continue
             _m[_key] = data
         return _m
@@ -35,8 +36,8 @@ class AppFactory:
         files = os.listdir(filepath)
         for fi in files:
             fi_d = os.path.join(filepath, fi)
-            if os.path.isdir(fi_d) and os.path.exists(f'{fi_d}/main.py'):
-                _module = fi_d.replace('/', '.') + '.main'
+            if os.path.isdir(fi_d) and os.path.exists(f"{fi_d}/main.py"):
+                _module = fi_d.replace("/", ".") + ".main"
                 try:
                     obj = importlib.import_module(_module)
                     for attr in obj.__dict__:
@@ -46,11 +47,17 @@ class AppFactory:
                         if not issubclass(_obj, Application):
                             continue
 
-                        if _obj.__name__ == "Application" \
-                                and _obj.__module__ == "website.applications.core.application":
+                        if (
+                            _obj.__name__ == "Application"
+                            and _obj.__module__
+                            == "website.applications.core.application"
+                        ):
                             continue
                         plog.debug(f"{_obj.__name__} Loaded.")
-                        AppFactory.MODULES[_obj.__name__] = {'module': _obj, "info": _obj.version().__dict__}
+                        AppFactory.MODULES[_obj.__name__] = {
+                            "module": _obj,
+                            "info": _obj.version().__dict__,
+                        }
 
                 except Exception as e:
                     print(f"Failed to import plugin：{_module}")
@@ -63,14 +70,17 @@ class AppFactory:
         return AppFactory.MODULES[name]
 
     @staticmethod
-    def get_application_module(name, config: NewWebSiteConfig, app_config: dict = None,
-                               storage_cls: Storage = None) -> Application:
-
+    def get_application_module(
+        name,
+        config: NewWebSiteConfig,
+        app_config: dict = None,
+        storage_cls: Storage = None,
+    ) -> Application:
         if storage_cls is None:
-            return AppFactory.MODULES[name]['module'](config, app_config)
+            return AppFactory.MODULES[name]["module"](config, app_config)
         else:
-            return AppFactory.MODULES[name]['module'](config, app_config, storage_cls)
+            return AppFactory.MODULES[name]["module"](config, app_config, storage_cls)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
