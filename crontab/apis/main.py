30dde177ebefec60550217f5efdb6faaee282d79
zmaplex <zmaplex@gmail.com>
2023-04-26 08:41:48 +0000

    Fixed: test bugs.

--- crontab/apis/main.py
+++ crontab/apis/main.py
@@ -4,16 +4,3 @@ from rest_framework.decorators import action
 from base.demo.serializers.main import MainSerializer
 from base.viewset import BaseReadOnlyModelViewSet
 from common.models import User
-
-
-class FileView(BaseReadOnlyModelViewSet):
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = User.objects.all()
-    serializer_class = MainSerializer
-
-    # 通过某个字段来获取具体的对象
-    lookup_field = "username"
-
-    @action(methods=["post"], detail=True, permission_classes=[permissions.AllowAny])
-    def notify(self, request, *args, **kwargs):
-        """ """
--- website/applications/app/flarum/test.py
+++ website/applications/app/flarum/test.py
@@ -1,11 +1,17 @@
 from django.test import TestCase
 
 from website.applications.app_factory import AppFactory
+from website.applications.core.dataclass import NewWebSiteConfig, WebServerTypeEnum
 
 
 class TestFlarum(TestCase):
     def test_get_application_module(self):
         app_factory = AppFactory
         app_factory.load()
-        config = {}
+        config = NewWebSiteConfig(
+            domain="test.uissh.com",
+            root_dir="/tmp/test.uissh.com",
+            web_server_type=WebServerTypeEnum.Nginx,
+        )
+
         app_factory.get_application_module("FlarumApplication", config)
